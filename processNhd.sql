524de2f6941747cb76f16e82d0b8f24292a1d55c
Nelson Minar <nelson@monkey.org>
2013-04-19 17:25:43 -0700

    simplify database creation

--- processNhd.sql
+++ processNhd.sql
@@ -1,29 +1,26 @@
--- transform the geometry to spherical mercator
-select AddGeometryColumn('nhdflowline', 'sphgeometry', 900913, 'MULTILINESTRING', 2);
-update nhdflowline set sphgeometry = ST_Transform(geom, 900913);
-
 -- create some indices on the important columns
-create index nhdflowline_sphgeometry_gist on nhdflowline using gist(sphgeometry);
 create index nhdflowline_comid_idx ON nhdflowline(comid);
 create index plusflowlinevaa_comid_idx ON plusflowlinevaa(comid);
 
 -- create a derived table that has the denormalized results for just our serving
--- PostGIS is smart enough to set up the spatial column
+-- Tilestache requires the geometry column be named 'geometry'
+-- PostGIS is smart enough to set up the spatial column metadata with this command
 create table rivers as
   select gid,
          nhdflowline.comid,
          cast(streamorde as int8) as strahler,
          gnis_name as name,
          ftype as ftype,
-         sphgeometry as geometry
+         ST_Transform(geom, 900913) as geometry
   from nhdflowline, plusflowlinevaa
   where nhdflowline.comid = plusflowlinevaa.comid
     and nhdflowline.ftype != 'Coastline';
 
 alter table rivers add primary key (gid);
 
--- spatial index on the served column
+-- indices on our derived table
 create index rivers_geometry_gist on rivers using gist(geometry);
+create index rivers_comid_idx ON rivers(comid);
 
 -- analyze to give the query planner appropriate hints
 vacuum analyze nhdflowline;
