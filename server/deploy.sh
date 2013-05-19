7d7f48ebc502308f4d2a67f464c2069fbbec085c
Nelson Minar <nelson@monkey.org>
2013-05-19 02:41:28 +0000

    nginx setup

--- server/deploy.sh
+++ server/deploy.sh
@@ -3,10 +3,18 @@
 # A deploy script for serving on somebits.com (with Nginx in front)
 # Not really an example to emulate, but you're welcome to take a look.
 
-cp -a server/serve.sh server/tilestache.cfg ~/rivers/
-sed "s%127.0.0.1:8000%www.somebits.com:8000%" < server/gunicorn.cfg.py >| ~/rivers/gunicorn.cfg.py
+# Server code
+cp -a server/serve.sh server/tilestache.cfg server/gunicorn.cfg.py ~/rivers/
 
+# Client code
 cp -a clients/lib clients/forkme.png clients/us-states.js /var/www/rivers/
 for c in clients/rivers-*.html; do
-    sed 's%localhost:8000%www.somebits.com:8000%g' < $c >| /var/www/rivers/`basename $c`
+    sed 's%localhost:8000%www.somebits.com:8001%g' < $c >| /var/www/rivers/`basename $c`
 done
+
+# Check nginx config
+cmp -s server/nginx-rivers.conf /etc/nginx/sites-enabled/rivers
+if [ $? -ne 0 ]; then
+    echo "Warning: server/nginx-rivers.conf is out of sync"
+    diff -u /etc/nginx/sites-enabled/rivers server/nginx-rivers.conf
+fi
